04926123bce2f3a70f034b31829cf3dc15965863
ferritinman <victorlouie.dev@gmail.com>
2023-03-17 08:12:33 +0000

    Copy pasta of db.py. At 30 min mark of last video

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,6 +1,11 @@
 import uuid
 from datetime import datetime, timedelta, timezone
+from lib.db import db
+
+
 class CreateActivity:
+  # def validations():
+
   def run(message, user_handle, ttl):
     model = {
       'errors': None,
@@ -49,3 +54,19 @@ class CreateActivity:
         'expires_at': (now + ttl_offset).isoformat()
       }
     return model
+
+    def create_activity(user_uuid, message, expires_at):
+      sql = f"""
+        INSERT INTO (
+          user_uuid,
+          message,
+          expires_at
+        ) VALUES (
+          '{user_uuid}',
+          '{message}',
+          '{expires_at}'
+
+        )
+      """
+
+      query_commit(sql)
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool, query_wrap_array
+from lib.db import db
 
 # "home.activities" name the tracer after the module/file (library.name)
 tracer = trace.get_tracer("home.activities")
@@ -10,35 +10,26 @@ class HomeActivities:
   def run(cognito_user_id=None):
     # logger.info('HomeActivities')
 
-
-
     # "home-activities-mock-data" is name of the span - super important
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
 
-      sql = query_wrap_array("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
+      results = db.query_array_json("""
+        SELECT
+          activities.uuid,
+          users.display_name,
+          users.handle,
+          activities.message,
+          activities.replies_count,
+          activities.reposts_count,
+          activities.likes_count,
+          activities.reply_to_activity_uuid,
+          activities.expires_at,
+          activities.created_at
+        FROM public.activities
+        LEFT JOIN public.users ON users.uuid = activities.user_uuid
+        ORDER BY activities.created_at DESC
       """)
-      print(sql)
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-      return json[0]
+      return results
