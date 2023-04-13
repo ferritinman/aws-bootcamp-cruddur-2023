7a10dc4bc22bfa028bea0c9a137ae62bbb08adc0
ferritinman <victorlouie.dev@gmail.com>
2023-04-13 07:43:39 +0000

    Based on my current cognito users, I'll have to change the username to bayko for now

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -282,7 +282,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'victor'
+  user_handle  = 'bayko'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
