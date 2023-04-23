de69c8c13af8aacd7cf4531f1eb893928707a8c5
ferritinman <victorlouie.dev@gmail.com>
2023-04-23 07:06:32 +0000

    Finally works! Remove the Bearer part out of the authorization token

--- aws/lambdas/lambda-authorizer/index.js
+++ aws/lambdas/lambda-authorizer/index.js
@@ -14,7 +14,7 @@ const jwtVerifier = CognitoJwtVerifier.create({
 exports.handler = async (event) => {
   console.log("request:", JSON.stringify(event, undefined, 2));
 
-  const jwt = event.headers.authorization;
+  const jwt = event.headers.authorization.replace('Bearer ', '');
   try {
     const payload = await jwtVerifier.verify(jwt);
     console.log("Access allowed. JWT payload:", payload);
