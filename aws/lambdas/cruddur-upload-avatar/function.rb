f4af9824b1fee8928612eb5b02a170fdbc6828bd
Victor Louie <victorlouie.dev@gmail.com>
2023-04-24 21:50:11 -0600

    Checkin file because I want to use gitpod now for bundle install

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -1,11 +1,23 @@
 require 'aws-sdk-s3'
 require 'json'
+require 'jwt'
 
 def handler(event:, context:)
-  puts event
+  token = event['headers']['authorization'].split(' ')[1]
+  puts({step: 'presignedurl', access_token: token}.to_json)
+
+  body_hash = JSON.parse(event["body"])
+  extension = body_hash["extension"]
+
+  decoded_token = JWT.decode token, nil, false
+  cognito_user_uuid = decoded_token[0]['sub']
+
   s3 = Aws::S3::Resource.new
   bucket_name = ENV["UPLOADS_BUCKET_NAME"]
-  object_key = 'mock.jpg'
+  object_key = "#{cognito_user_uuid}.#{extension}"
+
+  puts({object_key: object_key}.to_json)
+
 
   obj = s3.bucket(bucket_name).object(object_key)
   url = obj.presigned_url(:put, expires_in: 60 * 5)
